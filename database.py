73617a5f771b7d2cc5f19183df0e67302b8f5591
shawchen5242 <a1349717347@gmail.com>
2025-06-05 14:47:38 +0800

    remote test2

--- database.py
+++ database.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 # Database URL (replace with your PostgreSQL credentials)
 #using NeonDB as an example
+#need another database
 DATABASE_URL = "postgresql://neondb_owner:npg_VhGXzeoJca62@ep-still-brook-a1o2ku0m-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
 
 # SQLAlchemy setup for ORM (sync only)
